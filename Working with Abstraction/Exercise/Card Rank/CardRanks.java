f4e1e70c5e1282334291de5f5b349c26f0a82249
JoseluisKrus <84505835+JoseluisKrus@users.noreply.github.com>
2022-05-02 12:07:00 +0300

    Card Rank

---
+++ Working with Abstraction/Exercise/Card Rank/CardRanks.java
@@ -0,0 +1,19 @@
+package CardRank;
+
+public enum CardRanks {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING;
+    private int value;
+
+}
