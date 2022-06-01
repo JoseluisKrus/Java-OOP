c126608dbc79d8ebdeb235a6a968bc3886be21fb
JoseluisKrus <84505835+JoseluisKrus@users.noreply.github.com>
2022-06-01 13:47:17 +0300

    Animals

---
+++ Polymorphism/Lab/Animals/Animal.java
@@ -0,0 +1,31 @@
+public abstract class Animal {
+    private String name;
+    private String favoriteFood;
+
+
+    protected Animal(String name, String favoriteFood) {
+        setName(name);
+        setFavoriteFood(favoriteFood);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFavoriteFood() {
+        return favoriteFood;
+    }
+
+    public void setFavoriteFood(String favoriteFood) {
+        this.favoriteFood = favoriteFood;
+    }
+
+    public String explainSelf(){
+        return String.format("I am %s and my favourite food is %s",getName(), getFavoriteFood());
+    }
+
+}
